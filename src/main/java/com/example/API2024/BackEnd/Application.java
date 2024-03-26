41aac814d56226159ab372654fb435d64c0efd47
iagocpv <iago.csouza15@gmail.com>
2024-03-26 09:50:02 -0300

    configuracao back inicial

---
+++ src/main/java/com/example/API2024/BackEnd/Application.java
@@ -0,0 +1,13 @@
+package com.example.API2024.BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API2024/BackEnd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API2024.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
