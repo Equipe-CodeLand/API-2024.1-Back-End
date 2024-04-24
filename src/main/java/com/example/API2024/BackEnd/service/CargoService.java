db408936d13af8d79f7ad922b486da81dca822f0
Laura Gabriel <eulauragabriel@gmail.com>
2024-04-24 15:41:08 -0300

    cadastro de usuario com erro no cors

--- src/main/java/com/example/API2024/BackEnd/service/CargoService.java
+++ src/main/java/com/example/API2024/BackEnd/service/CargoService.java
@@ -6,14 +6,20 @@ import org.springframework.stereotype.Service;
 import com.example.API2024.BackEnd.model.Cargo;
 import com.example.API2024.BackEnd.repository.CargoRepository;
 
+import java.util.List;
+
 @Service
 public class CargoService {
 
-	@Autowired
-	CargoRepository cargoRepository;
-	
-	public Cargo buscarCargoPorId(Long id) throws Exception {
-		Cargo cargo = cargoRepository.findById(id).orElseThrow(() -> new Exception("Cargo não encontrado"));
-		return cargo;
-	}
+    @Autowired
+    private CargoRepository cargoRepository;
+
+    public List<Cargo> listarTodosCargos() {
+        return cargoRepository.findAll();
+    }
+
+    public Cargo buscarCargoPorId(Long id) throws Exception {
+        Cargo cargo = cargoRepository.findById(id).orElseThrow(() -> new Exception("Cargo não encontrado"));
+        return cargo;
+    }
 }
